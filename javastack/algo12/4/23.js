939d5dd29c358e34f180524fe4a94d55b2ec7f51
JacobPayne04 <Jacob.pa7ne@gmail.com>
2023-12-04 17:12:32 -0600

    initial commit

---
+++ javastack/algo12/4/23.js
@@ -0,0 +1,197 @@
+// https://www.geeksforgeeks.org/sorted-array-to-balanced-bst/
+
+/**
+ * Class to represent a Node in a Binary Search Tree (BST).
+ */
+class BSTNode {
+    /**
+     * Constructs a new instance of a BST node.
+     * @param {number} data The integer to store in the node.
+     */
+    constructor(data) {
+      this.data = data;
+      /**
+       * These properties are how this node is connected to other nodes to form
+       * the tree. Similar to .next in a SinglyLinkedList except a BST node can
+       * be connected to two other nodes. To start, new nodes will not be
+       * connected to any other nodes, these properties will be set after
+       * the new node is instantiated.
+       *
+       * @type {BSTNode|null}
+       */
+      this.left = null;
+      /** @type {BSTNode|null} */
+      this.right = null;
+    }
+  }
+  
+  /**
+   * Represents an ordered tree of nodes where the data of left nodes are <= to
+   * their parent and the data of nodes to the right are > their parent's data.
+   */
+  class BinarySearchTree {
+    constructor() {
+      /**
+       * Just like the head of a linked list, this is the start of our tree which
+       * branches downward from here.
+       *
+       * @type {BSTNode|null}
+       */
+      this.root = null;
+    }
+  
+    /**
+     * Determines if this tree is empty.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean} Indicates if this tree is empty.
+     */
+    isEmpty() {
+      return this.root === null;
+    }
+  
+    /**
+     * Retrieves the smallest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The smallest integer from this tree.
+     */
+    min(current = this.root) {
+      while (current && current.left) {
+        current = current.left;
+      }
+  
+      return current.data;
+    }
+  
+    /**
+     * Retrieves the smallest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The smallest integer from this tree.
+     */
+    minRecursive(current = this.root) {
+      return current && current.left
+        ? this.minRecursive(current.left)
+        : current.data;
+    }
+  
+    /**
+     * Retrieves the largest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The largest integer from this tree.
+     */
+    max(current = this.root) {
+      while (current && current.right) {
+        current = current.right;
+      }
+  
+      return current.data;
+    }
+  
+    /**
+     * Retrieves the largest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The largest integer from this tree.
+     */
+    maxRecursive(current = this.root) {
+      return current && current.right
+        ? this.maxRecursive(current.right)
+        : current.data;
+    }
+  
+    // Logs this tree horizontally with the root on the left.
+    print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
+      if (!node) {
+        return;
+      }
+  
+      spaceCnt += spaceIncr;
+      this.print(node.right, spaceCnt);
+  
+      console.log(
+        " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+        `${node.data}`
+      );
+  
+      this.print(node.left, spaceCnt);
+    }
+  }
+  
+  const emptyTree = new BinarySearchTree();
+  const oneNodeTree = new BinarySearchTree();
+  oneNodeTree.root = new BSTNode(10);
+  console.log("This is the min() for emptyTree: ", oneNodeTree.min());
+  console.log(
+    "This is the minRecursive() for emptyTree: ",
+    oneNodeTree.minRecursive()
+  );
+  
+  console.log("This is the max for emptyTree: ", oneNodeTree.max());
+  console.log(
+    "This is the maxRecursive() for emptyTree: ",
+    oneNodeTree.maxRecursive()
+  );
+  console.log("********************************");
+  
+  /* twoLevelTree
+          root
+          10
+        /   \
+      5     15
+  */
+  const twoLevelTree = new BinarySearchTree();
+  twoLevelTree.root = new BSTNode(10);
+  twoLevelTree.root.left = new BSTNode(5);
+  twoLevelTree.root.right = new BSTNode(15);
+  console.log("This is the min() for twoLevelTree: ", twoLevelTree.min());
+  console.log(
+    "This is the minRecursive() for twoLevelTree: ",
+    twoLevelTree.minRecursive()
+  );
+  
+  console.log("This is the max() for twoLevelTree: ", twoLevelTree.max());
+  console.log(
+    "This is the maxRecursive() for twoLevelTree: ",
+    twoLevelTree.maxRecursive()
+  );
+  console.log("********************************");
+  
+  /* threeLevelTree 
+          root
+          10
+        /   \
+      5     15
+    / \    / \
+  2   6  13  
+  */
+  const threeLevelTree = new BinarySearchTree();
+  threeLevelTree.root = new BSTNode(10);
+  threeLevelTree.root.left = new BSTNode(5);
+  threeLevelTree.root.left.left = new BSTNode(2);
+  threeLevelTree.root.left.right = new BSTNode(6);
+  threeLevelTree.root.right = new BSTNode(15);
+  threeLevelTree.root.right.left = new BSTNode(13);
+  
+  console.log("This is the min() for threeLEvelTree: ", threeLevelTree.min());
+  console.log(
+    "This is the minRecursive() for threeLEvelTree: ",
+    threeLevelTree.minRecursive()
+  );
+  
+  console.log("This is the max() for threeLEvelTree: ", threeLevelTree.max());
+  console.log(
+    "This is the maxRecursive() for threeLEvelTree: \n",
+    threeLevelTree.maxRecursive()
+  );
+  console.log("********************************");
