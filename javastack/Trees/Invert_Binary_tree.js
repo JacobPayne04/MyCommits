47c7e9f619f0895df59773e177d1aaca3adce8b2
jacob Payne <jacob.pa7ne@Gmail.com>
2024-05-04 19:48:42 -0500

    invert BST

---
+++ javastack/Trees/Invert_Binary_tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    
+    //make a traversal to store nodes and then traverse and store nodes
+    //make the root the root then next one is left and next one is right adn so on 
+    let res = [];
+
+
+    function dfs(node){
+        if(node == null) return;
+
+        dfs(node.left)
+        dfs(node.right)
+        res.push(node.val)
+    }
+   dfs(root)
+   
+   return res;
+};
+
+
+
+root = [4,2,7,1,3,6,9]
