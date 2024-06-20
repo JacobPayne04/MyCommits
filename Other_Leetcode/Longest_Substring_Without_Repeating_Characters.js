cf5baeeb29770052172e4235641e4bb016cdcb5d
jacob Payne <jacob.pa7ne@Gmail.com>
2024-06-19 22:08:00 -0500

    Longest Substring Without Repeating Characters

---
+++ Other_Leetcode/Longest_Substring_Without_Repeating_Characters.js
@@ -0,0 +1,22 @@
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+
+    for (let i = 0; i < s.length; i++) {
+
+        while (set.has(s[i])) {
+            set.delete(s[left])// adding to a set but deleting the set if we see reapeting chararcter
+            left++;
+        }
+        set.add(s[i]);// adding if its not in our set
+        maxSize = Math.max(maxSize, i - left + 1)//max size of the of the array updating each time//
+    }
+    return maxSize;
+}
+
+
+
