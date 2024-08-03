76081cc87b10ae33c760ccfc8ede1c35b33b59ba
JacobPayne04 <Jacob.pa7ne@gmail.com>
2024-08-03 15:38:42 -0500

    Longest Consecutive Sequence

--- Other_Leetcode/Longest_Consecutive_Sequence.js
+++ Other_Leetcode/Longest_Consecutive_Sequence.js
@@ -39,3 +39,26 @@ var longestConsecutive = function(nums) {
 
     return max;
 };
+
+
+// second solution O(n)
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+    const set = new Set(nums)
+    let longest = 0
+
+    for (let n of set) {
+        if (!set.has(n - 1)) {
+            let length = 1
+            while (set.has(n + length)) {
+                length++
+            }
+            longest = Math.max(length, longest)
+        }
+    }
+     return longest
+};
