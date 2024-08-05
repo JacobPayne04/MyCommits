c3b542a640cee27cae1902c15f2f47130119b594
JacobPayne04 <Jacob.pa7ne@gmail.com>
2024-08-04 21:57:49 -0500

    second solution

--- Other_Leetcode/Product_of_Array_Except_Self.js
+++ Other_Leetcode/Product_of_Array_Except_Self.js
@@ -20,3 +20,29 @@ var productExceptSelf = function(nums) {
     return result
 
 };
+
+
+//second solution
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const output =[];
+
+    const leftProducts =[];
+    const rightProducts =[];
+    leftProducts[0] = 1;
+    rightProducts[n - 1] = 1;
+
+    for (let i = 1; i < n; i++) {
+        leftProducts[i] = leftProducts[i - 1] * nums[i - 1];
+    }
+    
+    for (let i = n - 2; i >= 0; i--) {
+        rightProducts[i] = rightProducts[i + 1] * nums[i + 1];
+    }
+
+    for (let i = 0; i < n; i++) {
+        output[i] = leftProducts[i] * rightProducts[i];
+    }
+    
+    return output;
+};
