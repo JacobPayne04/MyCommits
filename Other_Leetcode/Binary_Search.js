582173305f2912701d0a3a4a0191b2413829ead1
jacob Payne <jacob.pa7ne@Gmail.com>
2024-08-27 23:26:35 -0400

    Binary seach sedcond solution

--- Other_Leetcode/Binary_Search.js
+++ Other_Leetcode/Binary_Search.js
@@ -13,3 +13,30 @@ var search = function(nums, target) {
       } 
       return answer
   };
+
+
+  //second solution
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let start = 0
+    let end = nums.length-1
+   
+   
+    while(start <= end){
+       let mid = Math.floor((start + end) / 2)
+       if(nums[mid] === target){
+           return mid
+       }
+   
+       if(target < nums[mid]){
+            end = mid - 1
+       }
+       else start = mid + 1
+    }
+    return -1
+   };
+   
