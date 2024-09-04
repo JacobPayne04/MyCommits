aa08d8d4b6fe8ff83cbcad0ffafabc408f6cf64f
jacob Payne <jacob.pa7ne@Gmail.com>
2024-09-03 21:50:06 -0500

    Best Time to Buy and Sell Stock second solution

--- Other_Leetcode/Best_Time_to_Buy_and_Sell_Stock.js
+++ Other_Leetcode/Best_Time_to_Buy_and_Sell_Stock.js
@@ -17,3 +17,35 @@ var maxProfit = function(prices) {
     
     return maxProfit;
 };
+
+
+
+
+
+
+
+// second solution
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */                                //       i
+ var maxProfit = function(prices) { //    [ [7][1][5][3][6][4] ] prices
+
+    let min = prices[0]
+    let max = min
+    let value = 0
+  
+    for(let i = 0; i < prices.length;i++){
+      if(i != prices.length-1 && prices[i] <= min){
+          min = prices[i]
+          max = min
+      } else if( prices[i] > max){
+          max = prices[i]
+      }
+      value = Math.max(value,max-min)
+    }
+    return value
+  };
+  
+    
--- Other_Leetcode/Find_Minimum in_Rotated_Sorted_Array.js
+++ Other_Leetcode/Find_Minimum in_Rotated_Sorted_Array.js
@@ -21,3 +21,26 @@ var findMin = function(nums) {
    return nums[left]
    
    };
+
+   // second solution 
+   var findMin = function(nums) {
+    //instantiat left and right and then when they have crossed con
+    
+    let left = 0
+    let right = nums.length-1
+  
+    
+    while(left < right){
+       let mid = Math.floor((left+right)/2);
+        
+        if(nums[mid] < nums[right]){
+            right = mid
+        
+           } else {
+            left = mid + 1
+           }
+          
+    }
+
+return nums[left]
+};
