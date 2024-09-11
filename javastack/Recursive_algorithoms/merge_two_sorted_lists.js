4ac62942c5132216c57bdd3d3c5f2d05037df68b
jacob Payne <jacob.pa7ne@Gmail.com>
2024-09-10 21:37:10 -0500

    Merge Two Sorted Lists second solution

--- javastack/Recursive_algorithoms/merge_two_sorted_lists.js
+++ javastack/Recursive_algorithoms/merge_two_sorted_lists.js
@@ -28,3 +28,33 @@ var mergeTwoLists = function(list1, list2) {
     return head
 };
 
+//second solution
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+    // Base cases: if one list is empty, return the other list
+    if (!list1) return list2;
+    if (!list2) return list1;
+
+    // Recursive comparison of list nodes
+    if (list1.val < list2.val) {
+        // list1 node is smaller, so it should be part of the result
+        list1.next = mergeTwoLists(list1.next, list2); // Recur with next of list1
+        return list1;
+    } else {
+        // list2 node is smaller, so it should be part of the result
+        list2.next = mergeTwoLists(list1, list2.next); // Recur with next of list2
+        return list2;
+    }
+};
