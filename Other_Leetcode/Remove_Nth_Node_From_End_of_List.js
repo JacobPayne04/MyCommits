072a6d3e57af7cf25fe1eb32be074fdd8c0f257b
jacob Payne <jacob.pa7ne@Gmail.com>
2024-09-24 21:02:39 -0400

    Remove Nth Node From End of List second try

--- Other_Leetcode/Remove_Nth_Node_From_End_of_List.js
+++ Other_Leetcode/Remove_Nth_Node_From_End_of_List.js
@@ -33,3 +33,29 @@ var removeNthFromEnd = function (head, n) {
    
    };
    
+
+   // second attempt was able to do it from understanding
+
+   var removeNthFromEnd = function (head, n) {
+      let dummy = new ListNode(0)// dummy list nodes where its tart wiht 0
+      dummy.next = head// the next to the dummy 0 node is head
+     
+     let fast = dummy// fast and slow starting at the dummy head
+     let slow = dummy
+     
+     for(let i = 0; i < n; i++){
+         fast = fast.next// fast will move till n
+     }
+     
+     while(fast.next !== null){//fast.next is not null we move fast adn slwo the same bc fast has already move n amount
+         fast = fast.next
+         slow = slow.next
+     }
+     slow.next = slow.next.next// connecting slow.next to the next nexst wihc is the node over hte nth node
+     
+     
+     
+     
+     return dummy.next// returning dummy.next give the orginal head 
+     };
+     
