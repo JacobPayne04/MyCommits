f1c6a88337e60ff8b2d57829bbc19a98b2d02d58
jacob Payne <jacob.pa7ne@Gmail.com>
2024-10-14 21:24:33 -0500

    Balanced Binary Tree Second Attempt

--- javastack/interveiw.js
+++ javastack/interveiw.js
@@ -2,15 +2,15 @@
    var findSecondMax = function(input,k){
   
    let count = 0
-   let max = 0
-   let curMax = 0
+   let max = input[0]
+   let curMax = input[0]
    let index = 0
 
 
 
    for(let i = 0; i < input.length;i++){
     if(count < k){
-        
+        console.log(curMax,max,input[i])
         if(curMax >= max){
             count++
             curMax = input[i]
@@ -23,9 +23,12 @@
             
    }
    
-return index
+    return index
    }
    
-   let input = [3,3,0,0,0,]
+   //let input = [3,3,0,0,0]
+   let input = [4,6,3,6,8,3,7,8]
+   //let input3 = [1,3,8,3,7,8,5,8]
    let k = 2
    console.log(findSecondMax(input,k));
+   
