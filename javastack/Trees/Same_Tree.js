743cfa6251b344e1dcbd145f6c203a99abb9eb65
jacob Payne <jacob.pa7ne@Gmail.com>
2024-10-15 21:49:51 -0500

    Same Tree

--- javastack/Trees/Same_Tree.js
+++ javastack/Trees/Same_Tree.js
@@ -42,3 +42,33 @@ var isSameTree = function(p, q) {
       
  return true
  };
+
+ //second solution 
+
+
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+
+    if(!p && !p){
+       return true
+    }
+   
+    if(p && q && p.val == q.val){
+       return(isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
+    } else {
+       return false
+    }
+   
+   };
