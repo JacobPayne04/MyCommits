559e52d7242ca98edb7cfb8cfea933619b1e3899
jacob Payne <jacob.pa7ne@Gmail.com>
2024-10-21 21:47:02 -0500

    Kth Smallest Element in a BST

--- javastack/Trees/Kth_Smallest_Element_in_a_BST.js
+++ javastack/Trees/Kth_Smallest_Element_in_a_BST.js
@@ -28,3 +28,36 @@ var kthSmallest = function(root, k) {
     res.sort((a,b)=>a-b)
 return res[k-1];
 };
+
+//second solution
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let stack = []
+  let cur = root
+
+  while(stack.length > 0 || cur !== null){
+    while(cur !== null){
+        stack.push(cur)
+        cur = cur.left
+    }
+    cur = stack.pop()
+    k--
+    if(k === 0){
+        return cur.val
+    }
+    cur = cur.right
+
+  }
+};  
